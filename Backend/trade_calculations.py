19f28cb7fd75e15eb699c1d328bbe25a424e40db
Your Name <dohnabenjamin@gmail.com>
2025-11-18 20:19:47 +0100

    Refactor trade balance calculation for clarity and consistency in output

--- Backend/trade_calculations.py
+++ Backend/trade_calculations.py
@@ -10,21 +10,11 @@ from Data.data_utils import load_card_data
 card_data = load_card_data()
 
 # attributes for testing
-# cards_player = ["Knight", "Archers"]
-# cards_opponent = ["Mega Knight", "Balloon"]
+cards_player = ["Knight", "Archers"]
+cards_opponent = ["Mega Knight", "Balloon"]
 
 def get_trade_balance(cards_player: list, cards_opponent: list) -> int:
     
-    # Calculate the total elixir cost for the player's cards
-    elixir_spent_player = sum(
-        int(card_data[card]['elixir']) 
-        for card in cards_player
-    )
-
-    for card in cards_player: 
-        print(f"{card} costs {card_data[card]['elixir']} elixir")
-    
-    print(f"both these cards cost {elixir_spent_player} elixir")
     
     # Calculate the total elixir cost for the opponent's cards
     elixir_spent_opponent = sum(
@@ -35,18 +25,32 @@ def get_trade_balance(cards_player: list, cards_opponent: list) -> int:
     for card in cards_opponent: 
         print(f"{card} costs {card_data[card]['elixir']} elixir")
     
-    print(f"both these cards cost {elixir_spent_opponent} elixir")
+    print(f"Total spent opponent: {elixir_spent_opponent} elixir")
+
+    # Calculate the total elixir cost for the player's cards
+    elixir_spent_player = sum(
+        int(card_data[card]['elixir']) 
+        for card in cards_player
+    )
+
+    for card in cards_player: 
+        print(f"{card} costs {card_data[card]['elixir']} elixir")
+    
+    print(f"Total spent player: {elixir_spent_player} elixir")
+
 
     # Determine the trade balance
     trade_balance = elixir_spent_opponent - elixir_spent_player
+
+    print(f"{elixir_spent_opponent} - {elixir_spent_player} = {trade_balance}")
+    print(f"-> the absolute value is {abs(trade_balance)}")
     
     return trade_balance
     
 
-def get_trade_outcome(trade_balance: int) -> str: 
+def get_trade_balance_sign(trade_balance: int) -> str: 
     if trade_balance > 0:
         return "positive"
     if trade_balance < 0: 
         return "negative"
     return "neutral"
-
